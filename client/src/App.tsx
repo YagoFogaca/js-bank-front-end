2a92c2b8c043077ac92efaa6aca2404f2178d7dc
eduardo <95304267+bruce-69@users.noreply.github.com>
2023-04-11 21:33:42 -0300

    imail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { GlobalStyle } from './global.style';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PageCpf } from './pages/page-cpf';
 import { PagePersonalInfo } from './pages/personal-info';
+import { PageEmail } from './pages/page-email';
 
 export function App() {
     return (
@@ -14,6 +15,7 @@ export function App() {
                         path="/personal-info"
                         element={<PagePersonalInfo />}
                     />
+                    <Route path="/email" element={<PageEmail />} />
                 </Routes>
             </BrowserRouter>
         </>
