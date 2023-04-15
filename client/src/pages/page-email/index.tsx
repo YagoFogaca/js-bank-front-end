b66331a2938cf4c871b9753f3044c61f2c3ae668
Yago Fogaça <yagofogaca20@gmail.com>
2023-04-15 10:11:23 -0300

    feat: reformulção do pagina de email

--- client/src/pages/page-email/index.tsx
+++ client/src/pages/page-email/index.tsx
@@ -1,43 +1,36 @@
 import { useContext, useState } from 'react';
-import { CardRegistration } from '../../components/card-registration/index.card-registration';
-import * as C from '../../styled-components/btns/index.btn';
-import { BoxInput, Input } from '../../styled-components/inputs/index.input';
-import { Label } from '../../styled-components/label/index.label';
+import { useNavigate } from 'react-router-dom';
 import { Api } from '../../utils/api/api';
 import { UserContext } from '../../contexts/user.context';
-import { useNavigate } from 'react-router-dom';
+import { CardRegistration } from '../../components/card-registration/index.card-registration';
+import { BoxInput, Input } from '../../styled-components/inputs/index.input';
+import { Label } from '../../styled-components/label/index.label';
+import { VarianteSpanError } from '../../styled-components/span/index.span';
+import * as C from '../../styled-components/btns/index.btn';
+import { ValidateInfos } from '../../utils/types/index.props';
 
 export function PageEmail() {
-    const [codeSent, setCodeSent] = useState(false);
-    const { user, setUser } = useContext(UserContext);
     const navigate = useNavigate();
+    const { user, setUser } = useContext(UserContext);
+    const [validateDocumentNumber, setValidateDocumentNumber] =
+        useState<ValidateInfos>({
+            message: '',
+            valid: false,
+        });
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         const emailAddress = event.currentTarget.emailAddress.value;
-
         try {
+            // await Api.sendEmailCode(emailAddress);
             setUser({ ...user, emailAddress });
-            await Api.sendEmailCode(emailAddress);
-            setCodeSent(true);
+            navigate('/verify-email');
         } catch (error) {
-            // Mensagem de e-mail já existente no banco de dados
-        }
-    };
-
-    const verifyEmailCode = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        const emailAddress = user.emailAddress;
-
-        const emailCode = event.currentTarget.emailCode.value;
-
-        try {
-            await Api.verifyEmailCode({ emailAddress, emailCode });
-            navigate('/informacoes-residenciais');
-        } catch (error) {
-            // Mensagem de código de validação inválido
+            setValidateDocumentNumber({
+                message: 'E-mail já registrado',
+                valid: true,
+            });
         }
     };
 
@@ -45,64 +38,45 @@ export function PageEmail() {
         event: React.ChangeEvent<HTMLInputElement>,
     ) => {
         if (
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+            !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
                 event.currentTarget.value,
             )
         ) {
-            console.log('É valido: ', event.currentTarget.value);
-        } else {
-            console.log('Invalido: ', event.currentTarget.value);
+            setValidateDocumentNumber({
+                message: 'E-mail invalido',
+                valid: true,
+            });
+        } else if (validateDocumentNumber.valid) {
+            console.log('Teste');
+            setValidateDocumentNumber({
+                message: '',
+                valid: false,
+            });
         }
     };
 
     return (
         <>
-            {!codeSent ? (
-                <CardRegistration>
-                    <form onSubmit={handleSubmit}>
-                        <BoxInput>
-                            <Input
-                                required
-                                id="emailAddress"
-                                type="email"
-                                onChange={handleEmailCodeChange}
-                            />
-                            <Label>E-mail</Label>
-                        </BoxInput>
+            <CardRegistration>
+                <form onSubmit={handleSubmit}>
+                    <VarianteSpanError visible={validateDocumentNumber.valid}>
+                        {validateDocumentNumber.message}
+                    </VarianteSpanError>
+                    <BoxInput>
+                        <Input
+                            required
+                            id="emailAddress"
+                            type="email"
+                            onChange={handleEmailCodeChange}
+                        />
+                        <Label>E-mail</Label>
+                    </BoxInput>
 
-                        <C.BoxBtns>
-                            <C.Btn>Seguir</C.Btn>
-                        </C.BoxBtns>
-                    </form>
-                </CardRegistration>
-            ) : (
-                <CardRegistration>
-                    <form onSubmit={verifyEmailCode}>
-                        <p>
-                            Enviamos um código de validação para o e-mail
-                            informado. Ele tem duração de 15 minutos.
-                        </p>
-                        <br></br>
-                        <BoxInput>
-                            <Input required id="emailCode" type="text" />
-                            <Label>Insira o código de validação</Label>
-                        </BoxInput>
-
-                        <C.VarianteBoxBtns>
-                            <C.VarianteButton
-                                type="button"
-                                color="true"
-                                onClick={() => {
-                                    setCodeSent(false);
-                                }}
-                            >
-                                Reenviar código
-                            </C.VarianteButton>
-                            <C.VarianteButton>Seguir</C.VarianteButton>
-                        </C.VarianteBoxBtns>
-                    </form>
-                </CardRegistration>
-            )}
+                    <C.BoxBtns>
+                        <C.Btn>Seguir</C.Btn>
+                    </C.BoxBtns>
+                </form>
+            </CardRegistration>
         </>
     );
 }
