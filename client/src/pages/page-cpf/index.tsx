09ceffab4cc1f37bc0a2a3cb2c2deb8f21301b69
Yago Fogaça <yagofogaca20@gmail.com>
2023-04-18 09:46:20 -0300

    feat: implementação da chamada da api na pagina de cpf

--- client/src/pages/page-cpf/index.tsx
+++ client/src/pages/page-cpf/index.tsx
@@ -25,6 +25,7 @@ export function PageCpf() {
         const documentNumber = event.currentTarget.documentNumber.value;
 
         try {
+            await Api.findDocumentNumber(documentNumber);
             setUser({ ...user, documentNumber });
             navigate('/informacoes-pessoais');
         } catch (error) {
