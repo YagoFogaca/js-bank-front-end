7f24f853918358ec5179936b4fb82a2c2f24e892
Yago Fogaça <yagofogaca20@gmail.com>
2023-04-21 14:29:48 -0300

    feat: reestruturação da página de informações email

--- client/src/pages/page-email/index.tsx
+++ client/src/pages/page-email/index.tsx
@@ -3,20 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { Api } from '../../utils/api/api';
 import { UserContext } from '../../contexts/user.context';
 import { CardRegistration } from '../../components/card-registration/index.card-registration';
-import { BoxInput, Input } from '../../styled-components/inputs/index.input';
+import { Input } from '../../styled-components/inputs/index.input';
 import { Label } from '../../styled-components/label/index.label';
-import { VarianteSpanError } from '../../styled-components/span/index.span';
 import * as C from '../../styled-components/btns/index.btn';
-import { ValidateInfos } from '../../utils/types/index.props';
+
+import * as TI from '../../styled-components/text-information/index.text';
+import * as TE from '../../styled-components/span/index.span';
 
 export function PageEmail() {
     const navigate = useNavigate();
     const { user, setUser } = useContext(UserContext);
-    const [validateDocumentNumber, setValidateDocumentNumber] =
-        useState<ValidateInfos>({
-            message: '',
-            valid: false,
-        });
+    const [emailCheck, setEmailCheck] = useState(false);
+    const [validateEmail, setValidateEmail] = useState(false);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -27,50 +25,53 @@ export function PageEmail() {
             setUser({ ...user, emailAddress });
             navigate('/verify-email');
         } catch (error) {
-            setValidateDocumentNumber({
-                message: 'E-mail já registrado',
-                valid: true,
-            });
+            setValidateEmail(true);
         }
     };
 
     const handleEmailCodeChange = (
         event: React.ChangeEvent<HTMLInputElement>,
     ) => {
-        if (
-            !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
-                event.currentTarget.value,
-            )
-        ) {
-            setValidateDocumentNumber({
-                message: 'E-mail invalido',
-                valid: true,
-            });
-        } else if (validateDocumentNumber.valid) {
-            console.log('Teste');
-            setValidateDocumentNumber({
-                message: '',
-                valid: false,
-            });
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!regex.test(event.currentTarget.value)) {
+            setEmailCheck(true);
+        } else if (emailCheck) {
+            setEmailCheck(false);
         }
     };
 
     return (
         <>
             <CardRegistration>
+                {validateEmail ? (
+                    <TE.TextErrorVariante>
+                        O E-mail já está registrado!
+                    </TE.TextErrorVariante>
+                ) : (
+                    <TI.Text>Agora é a hora do seu E-mail</TI.Text>
+                )}
                 <form onSubmit={handleSubmit}>
-                    <VarianteSpanError visible={validateDocumentNumber.valid}>
-                        {validateDocumentNumber.message}
-                    </VarianteSpanError>
-                    <BoxInput>
+                    <div>
+                        <Label>E-mail</Label>
                         <Input
+                            placeholder="jsbank@jsbank.com"
                             required
                             id="emailAddress"
                             type="email"
+                            className={emailCheck ? 'error' : ''}
+                            autoComplete="off"
                             onChange={handleEmailCodeChange}
                         />
-                        <Label>E-mail</Label>
-                    </BoxInput>
+                        {emailCheck ? (
+                            <TE.TextError visible={emailCheck}>
+                                E-mail invalido!
+                            </TE.TextError>
+                        ) : (
+                            <TI.TextInformation>
+                                Seu e-mail pessoal, aquele que você mais usa.
+                            </TI.TextInformation>
+                        )}
+                    </div>
 
                     <C.BoxBtns>
                         <C.Btn>Seguir</C.Btn>
