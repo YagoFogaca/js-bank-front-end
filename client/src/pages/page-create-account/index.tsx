031c6dd7ff25e6dc8d6592a9e261360e53fc8440
Yago Fogaça <yagofogaca20@gmail.com>
2023-04-26 14:17:07 -0300

    feat: troca de mensagens durante a criação da conta

--- client/src/pages/page-create-account/index.tsx
+++ client/src/pages/page-create-account/index.tsx
@@ -5,49 +5,54 @@ import { CardRegistration } from '../../components/card-registration/index.card-
 import { Loading } from '../../components/loading/index.loading';
 import { VarianteText } from '../../styled-components/text-information/index.text';
 import { CgCheck } from 'react-icons/cg';
+import { BiError } from 'react-icons/bi';
 
 export function CreateAccount() {
     const { user } = useContext(UserContext);
-
-    const createUser = async () => {
-        try {
-            return await Api.createUser(user);
-            // setMsg('Obrigador por escolher fazer parte da familia JSBANK');
-            // setCreatedAccount(true);
-            // setMsg('Conta criada com sucesso. Clique aqui para fazer login.');
-            // const secondTimeoutId = setTimeout(() => {
-            //     setCreatedAccount(true);
-            //     setMsg(
-            //         'Conta criada com sucesso. Clique aqui para fazer login.',
-            //     );
-            // }, 10000);
-
-            // return () => clearTimeout(secondTimeoutId);
-        } catch (error) {
-            console.log(error);
-            setMsg('Deu erro ai otario.');
-        }
-    };
-
     const [createdAccount, setCreatedAccount] = useState(false);
+    const [errorCreatedAccount, setErrorCreatedAccount] = useState(false);
     const [msg, setMsg] = useState(
         'Estamos processando suas informações. Aguarde!',
     );
 
+    const createUser = async () => {
+        try {
+            await Api.createUser(user);
+            setCreatedAccount(true);
+            setMsg(
+                'Sua conta foi criada com sucesso. Clique aqui para fazer login.',
+            );
+        } catch (error) {
+            console.log(error);
+            setErrorCreatedAccount(true);
+            setMsg(
+                'Ocorreu um erro ao criar sua conta. Revise suas informações.',
+            );
+        }
+    };
+
     useEffect(() => {
-        console.log(createUser());
-    }, [user]);
+        createUser();
+    }, []);
 
     return (
         <CardRegistration>
-            {createdAccount ? (
+            {createdAccount || errorCreatedAccount ? (
                 <>
                     <div style={{ margin: '0 auto', width: 'fit-content' }}>
-                        <CgCheck
-                            size={62}
-                            color="green"
-                            style={{ margin: '0 auto' }}
-                        />
+                        {errorCreatedAccount ? (
+                            <BiError
+                                size={62}
+                                color="red"
+                                style={{ margin: '0 auto' }}
+                            />
+                        ) : (
+                            <CgCheck
+                                size={62}
+                                color="green"
+                                style={{ margin: '0 auto' }}
+                            />
+                        )}
                     </div>
                     <VarianteText>{msg}</VarianteText>
                 </>
