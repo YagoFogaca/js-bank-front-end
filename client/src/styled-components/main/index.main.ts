647d9fa4c173d24a9280bef6641c52c14ed9fb3a
Yago Fogaça <yagofogaca20@gmail.com>
2023-04-27 15:19:46 -0300

    fix: caminho relativo do banner

--- client/src/styled-components/main/index.main.ts
+++ client/src/styled-components/main/index.main.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Main = styled.main`
     width: 100%;
     min-height: calc(100vh - 20px);
-    background-image: url(./src/assets/imgs/banner-0.jpg);
+    background-image: url(/imgs/banner-0.jpg);
     background-repeat: no-repeat;
     background-position: center;
 `;
