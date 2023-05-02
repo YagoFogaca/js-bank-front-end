323e828ee10860de730ec0cf3b40125f5149e968
yagoFogaca <yagofogaca20@gmail.com>
2023-05-02 15:42:33 -0300

    feat: implementação da funcionalidade de conferencia de dados

--- client/src/pages/page-check-personal/index.tsx
+++ client/src/pages/page-check-personal/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../contexts/user.context';
+import { UserContextType } from '../../utils/types/index.context';
 import { CardRegistration } from '../../components/card-registration/index.card-registration';
 import { Text } from '../../styled-components/text-information/index.text';
 import { BoxInput, Input } from '../../styled-components/inputs/index.input';
@@ -8,7 +9,6 @@ import { Label } from '../../styled-components/label/index.label';
 import { PersonalInformation } from '../../components/personal-information/index.personal-information';
 import { AddressInformation } from '../../components/address-information/index.address-information';
 import * as C from '../../styled-components/btns/index.btn';
-import { Api } from '../../utils/api/api';
 
 export function PageCheckPersonal() {
     const { user, setUser } = useContext(UserContext);
@@ -20,9 +20,30 @@ export function PageCheckPersonal() {
     });
 
     const address = user.homeAddress.split(',');
-
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+
+        const street = event.currentTarget.street.value;
+        const number = event.currentTarget.number.value;
+        const complement = event.currentTarget.complement.value;
+        const city = event.currentTarget.city.value;
+        const state = event.currentTarget.state.value;
+        const homeAddress = `${city},${state},${street},${number}${
+            complement ? ',' + complement : ''
+        }`;
+
+        const checkPersonal: UserContextType = {
+            documentNumber: event.currentTarget.documentNumber.value,
+            fullName: event.currentTarget.fullName.value,
+            birthDate: event.currentTarget.birthDate.value,
+            phoneNumber: event.currentTarget.phoneNumber.value,
+            zipCode: event.currentTarget.zipCode.value,
+            homeAddress: homeAddress,
+            emailAddress: user.emailAddress,
+            accessPassword: user.accessPassword ?? '',
+        };
+        const userCopy = Object.assign(user, checkPersonal);
+        setUser(userCopy);
 
         navigate('/create-account/password');
     };
@@ -35,6 +56,7 @@ export function PageCheckPersonal() {
                 <BoxInput>
                     <Label>CPF</Label>
                     <Input
+                        disabled
                         defaultValue={user.documentNumber}
                         placeholder="000.000.000-00"
                         required
